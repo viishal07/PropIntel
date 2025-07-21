e3b0919752d61783e19ca7bae85829c138c8b46b
Viishal Gowtham <viishal514@gmail.com>
2025-07-21 13:35:51 +0530

    Update frontend: set API base URL for production

--- client/src/pages/Underwrite.js
+++ client/src/pages/Underwrite.js
@@ -25,7 +25,7 @@ const propertyTypes = [
   'Land',
 ];
 
-const API_BASE = 'http://localhost:5000/api';
+const API_BASE = process.env.REACT_APP_API_BASE;
 
 const Underwrite = () => {
   const location = useLocation();
